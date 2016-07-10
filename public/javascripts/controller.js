80fe969d1aaa290b26366af53b40b4bfe31fb44f
xLMSxVerde <benedictkempkes@googlemail.com>
2016-07-10 13:26:14 +0200

    button
    button structure

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,6 +1,26 @@
+// controller
+//
+
 app.controller('page-ctrl', function($scope, $http) {
   $http.get('data/fm/series.json')
        .then(function(res){
           $scope.FMSerien = res.data;
         });
+});
+
+
+// directives
+//
+
+app.directive('stay', function(){
+  return {
+    restrict : "C",
+      link: function(scope, element) {
+        element.bind("click" , function(e){
+          console.log(element.parent().parent().find("button"));
+          element.parent().parent().find("button").removeClass("enabled");
+          element.addClass("enabled");
+        });
+      }
+  }
 });
